9b8ddd58086cf8d430f6e3b07df935ae097c59ef
Quynh-Chi Nguyen <qcnguyen345@gmail.com>
2019-12-17 22:43:42 -0700

    Finishing up Commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+Final Project:A To-Do List
+
+Target Audience:
+    -Someone that needs a quick to-do list for a work session while still wanting to be organized
+Problem You Will Solve:
+    -The need for a to-do list with a lot of customization
+Why It Needs to Be Solved:
+    -A lot of to-do lists online don't have many options in terms of organization and aesthetics. 
+
+Features Included: 
+    -Adding tasks
+    -Change the color of the task
+        -One of the options has flashing colors 
+    -Delete the task if needed
+    -Check off the task once completed
+    -Hover over the checkbox to see the due date
+
+URL: https://nguyenqc345.github.io/finalwebsite/
--- final.js
+++ final.js
@@ -1,22 +1,69 @@
 
-function ready(){
-    add.onclick = function (){
+add.onclick = function (){
         let item = document.getElementById("input").value;
+        let item2 = document.getElementById("input2").value;
         let elem = document.createElement('li');
-        elem.innerHTML = item;
+        elem.innerHTML = item
+        elem.setAttribute('id', "color");
         let button = document.createElement('span');
+        let check = document.createElement("input")
+        check.setAttribute("type", "checkbox");
+        check.innerHTML = "";
         button.innerHTML = "X";
         button.onclick = function(){
             elem.remove()
             button.remove()
         };
-        button.className = "close"
+        let menu = document.createElement('select');
+        menu.setAttribute('id', "cool")
+        let color0 = "<option value = 0>Select Color</option>"
+        let color1 = "<option value = 1>Gray</option>";
+        let color2 = "<option value = 2>Purple</option>";
+        let color3 = "<option value = 3>Important</option>";
+            menu.innerHTML= color0 + color1 + color2 + color3;
+        document.addEventListener("input", function (event){
+            if (event.target.id !== "cool") return;
+                if (event.target.value === '0') {
+                document.getElementById('color').style.background = "cornsilk";
+                }
+                if (event.target.value === '1') {
+                    document.getElementById('color').style.background = "#eee";
+                }
+                if (event.target.value === '2') {
+                    document.getElementById('color').style.background = "#b581e3";
+                }
+                if (event.target.value === '3') {
+                    let x = 0 
+                    setInterval(() =>{
+                        if (x % 2 == 0){
+                            document.getElementById('color').style.background = "#b581e3";}
+                        if (x % 2 == 1){
+                            document.getElementById('color').style.background = "#eee";}
+                        x++;
+                    },500)
+                }
+        }, false);
+        button.className = "close";
         document.getElementById("input").value="";
+        document.getElementById("input2").value=""
+        check.onclick = function(){
+            elem.className = "checked"
+            document.getElementById('color').style.background = "gray";
+        }
         list.append(elem);
         elem.append(button);
+        elem.append(menu);
+        elem.prepend(check);
+
+        check.onmouseover = function(){
+            elem.innerHTML = "Due Date:" + item2
+        };
+        elem.onmouseout = function(){
+            elem.innerHTML = item
+            list.append(elem);
+            elem.append(button);
+            elem.append(menu);
+            elem.prepend(check);
+        }
         
     };
-
-}
-
-document.addEventListener("DOMContentLoaded", ready);
